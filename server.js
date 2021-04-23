a11e74792fd50c4938137be958642f7d12995490
thierry <thierrypoupon@hotmail.com>
2021-04-23 22:55:49 +0200

    init project

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+
+const PORT = process.env.PORT || 8000;
+http.listen(PORT, () => {
+    const date = new Date();
+    console.log(`${ date.getHours() }H${ date.getMinutes() } on port : ${ PORT }`);
+});
