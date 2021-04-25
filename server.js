4ff1b5ba34d9017b357f402c6f622c9dc1c56b17
thierry <thierrypoupon@hotmail.com>
2021-04-25 20:56:39 +0200

    working on th front Home page

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8001;
 http.listen(PORT, () => {
     const date = new Date();
     console.log(`${ date.getHours() }H${ date.getMinutes() } on port : ${ PORT }`);
