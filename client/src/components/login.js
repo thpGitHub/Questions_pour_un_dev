128d8fb00ba550a5c63c4cb686855a22b5b5f566
thierry <thierrypoupon@hotmail.com>
2021-04-29 15:50:41 +0200

    create login page

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -4,8 +4,14 @@ const Login = () => {
 
     return (
         <div>
-
-            <p id="p">Login Page</p>
+            <form id="login" action="/verify_pseudo_pwd" method="post">
+                <h1>LOGIN</h1>
+                <div id="message"></div>
+                <input type="text"     name="pseudo" placeholder="Pseudo"/>
+                <input type="password" name="pwd"    placeholder="Password"/>
+                <input type="submit"   name="submit" value="Login"/>
+                <a href="/inscription">Create an Account</a>
+            </form>
         </div>
     );
 }
