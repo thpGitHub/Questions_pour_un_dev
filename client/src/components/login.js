9e05016ea3a5c567fb7e8cbe9569971b282662a7
thierry <thierrypoupon@hotmail.com>
2021-05-10 23:16:56 +0200

    when on click login button test redirect on choose game page

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,10 +1,20 @@
 import './login.css';
+import { useHistory } from 'react-router-dom';
 
 const Login = () => {
 
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Enter in handleSummit');
+        history.push('/choose_game');
+    }
+
     return (
         <div>
-            <form id="login" action="/verify_pseudo_pwd" method="post">
+            {/* <form id="login" action="/verify_pseudo_pwd" method="post"> */}
+            <form id="login" onSubmit={ e=> handleSubmit(e)}>
                 <h1>LOGIN</h1>
                 <div id="message_login"></div>
                 <input type="text"     name="pseudo" placeholder="Pseudo" autoComplete="off" spellCheck="false"/>
