495a190cdf3032446809e67127111c1fe512c1a8
thierry <thierrypoupon@hotmail.com>
2021-05-13 22:25:02 +0200

    fix cors in file server.js

--- database_connect.js
+++ database_connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connection = process.env.DB_HOST_ATLAS;
-mongoose.connect(connection,{ useNewUrlParser: true,
+mongoose.connect(connection.toString(),{ useNewUrlParser: true,
                               useUnifiedTopology: true,
                               useFindAndModify: false})
     .then(() => console.log("Database Connected Successfully"))
