f78461547ef2f0ff4f29ae77732c173c84ba27ed
thierry <thierrypoupon@hotmail.com>
2021-05-13 23:01:09 +0200

    fix cors in file server.js

--- database_connect.js
+++ database_connect.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// const connection = process.env.DB_HOST_ATLAS.toString();
 const connection = process.env.DB_HOST_ATLAS;
-mongoose.connect(connection.toString(),{ useNewUrlParser: true,
+// console.log(connection);
+mongoose.connect(connection,{ useNewUrlParser: true,
                               useUnifiedTopology: true,
                               useFindAndModify: false})
     .then(() => console.log("Database Connected Successfully"))
--- server.js
+++ server.js
@@ -2,23 +2,23 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const path = require('path');
-// const cors = require('cors');
+const cors = require('cors');
 require('./database_connect');
 // require('dotenv').config();//*********** */
 
-app.use((req, res, next) => {
-    //Permet d'accéder à notre API depuis n'importe quelle origine ('*')
-   res.setHeader('Access-Control-Allow-Origin', '*');
-    //Permet d'ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc.) ;
-   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    //Permet d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    // Permet de passer au middleware suivant 
-   next();
-});
+// app.use((req, res, next) => {
+//     //Permet d'accéder à notre API depuis n'importe quelle origine ('*')
+//    res.setHeader('Access-Control-Allow-Origin', '*');
+//     //Permet d'ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc.) ;
+//    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//     //Permet d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
+//    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//     // Permet de passer au middleware suivant 
+//    next();
+// });
 
+app.use(cors());
 
-// app.use(cors());
 // API ou controllers
 const login = require('./controllers/loginController');
 app.use('/controllers/loginController', login);
